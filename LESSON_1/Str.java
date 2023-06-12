8906762c007ff44959a18d9f0d24d2d8dd0c7a7a
ath08 <ath0807@gmail.com>
2023-06-13 00:52:50 +0400

    LESSON_1 Strings

---
+++ LESSON_1/Str.java
@@ -0,0 +1,17 @@
+class Str {
+    public static void main(String[] args){
+        String text = "My name is ";
+        String name = "Tatev";
+        String fullText = text + name;
+
+        System.out.println(fullText);
+
+        String welcomeSpeech = "Hello";
+
+        System.out.println(welcomeSpeech + " " + name);
+
+
+
+    }    
+
+}
